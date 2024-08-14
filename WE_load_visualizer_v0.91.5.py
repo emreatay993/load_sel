3103beb9f88b1c884c53b0df25862e51952af7a9
EmreAtay <63810700+emreatay993@users.noreply.github.com>
2024-08-14 17:37:49 +0300

    Update WE_load_visualizer_v0.91.5.py

--- WE_load_visualizer_v0.91.5.py
+++ WE_load_visualizer_v0.91.5.py
@@ -1495,7 +1495,7 @@ class WE_load_plotter(QWidget):
                         self.spectrum_canvas.draw()
 
                         plt.close(fig)  # Close the figure after drawing to save memory
-                    else:
+                    elif self.spectrum_checkbox.isChecked():
                         # Apply FFT on the uniform time grid data
                         fft_df = rolling_fft(self.working_df_tab1[[value_col]], num_slices=400, add_resultant=True)
                         heatmap = spectrum_over_time(fft_df, plot_type=plot_type, freq_max=None,
